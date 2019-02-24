38df004c9f16c65a2548b73602bcdc9adce0a053
Bharat Middha <bmiddha99@gmail.com>
2019-02-23 21:47:39 -0600

    Created recipe interface and class

---
+++ src/interfaces/Recipe.ts
@@ -0,0 +1,35 @@
+export interface Ingredient {
+    quantity: number;
+    units: string;
+    name: string;
+    special: string;
+}
+
+export interface Direction {
+    step: number;
+    info: string;
+}
+
+enum Level {
+    Easy,
+    Medium,
+    Hard,
+}
+
+export interface Recipe {
+    ingredients: Array<Ingredient>;
+    prepTime: number;
+    cookTime: number;
+    name: string;
+    image: string;
+    directions: Array<Direction>;
+    notes: string;
+    links: Array<string>;
+    categories: string;
+    yield: number;
+    level: Level;
+}
+
+export class Recipe implements Recipe {}
+export class Ingredient implements Ingredient {}
+export class Direction implements Direction {}
